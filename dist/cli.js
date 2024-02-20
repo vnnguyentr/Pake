9d4ce6585af895143d780f2a99ec58c2ff788cf8
Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>
2024-02-20 22:55:05 +0700

    Update cli.js
    Signed-off-by: Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>

--- dist/cli.js
+++ dist/cli.js
@@ -830,7 +830,7 @@ const DEFAULT_PAKE_OPTIONS = {
     fullscreen: false,
     resizable: true,
     transparent: false,
-    userAgent: '',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     showMenu: false,
     showSystemTray: false,
     multiArch: false,
