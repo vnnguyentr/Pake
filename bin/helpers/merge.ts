5856aa452748689dba9d9f049656a74728d74fbb
Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>
2024-02-20 23:32:19 +0700

    Update merge.ts
    Signed-off-by: Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>

--- bin/helpers/merge.ts
+++ bin/helpers/merge.ts
@@ -13,7 +13,7 @@ export async function mergeConfig(url: string, options: PakeAppOptions, tauriCon
     height,
     fullscreen,
     transparent,
-    userAgent,
+    userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     showMenu,
     showSystemTray,
     systemTrayIcon,
