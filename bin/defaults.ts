ea06f08dbe4d0840fcb8c47c51c2d0cbb4f1d18a
Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>
2024-02-20 23:48:18 +0700

    Update defaults.ts
    Signed-off-by: Nguyen Truong <101310397+vnnguyentr@users.noreply.github.com>

--- bin/defaults.ts
+++ bin/defaults.ts
@@ -7,7 +7,7 @@ export const DEFAULT_PAKE_OPTIONS: PakeCliOptions = {
   fullscreen: false,
   resizable: true,
   transparent: false,
-  userAgent: '',
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
   showMenu: false,
   showSystemTray: false,
   multiArch: false,
